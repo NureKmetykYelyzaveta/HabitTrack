7918cf28c41b3aefc772344790f9470704a3ba54
NurePoturaikoNazar <nazar.poturaiko@nure.ua>
2025-10-22 22:59:44 +0300

    Remove ignored files from repository

--- database/users_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    UserId UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    Username VARCHAR(50) NOT NULL,
-    Email VARCHAR(150) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL,
-    CreatedAt TIMESTAMP DEFAULT NOW(),
-    Role VARCHAR(20) NOT NULL DEFAULT 'user',
-    AvatarUrl TEXT
-);
